b6f06be64725fef0d7afd7bbd6b8648f3fe10b59
kevin adonis figueroa gonzales <kfigueroa@botpro.ai>
2024-02-18 12:54:39 -0400

    --

--- Prueba Defontana/Context/VentasContext.cs
+++ Prueba Defontana/Context/VentasContext.cs
@@ -27,7 +27,6 @@ namespace Prueba_Defontana.Context
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Server=lab-defontana-202310.caporvnn6sbh.us-east-1.rds.amazonaws.com,1433;Database=Prueba;User Id=ReadOnly;Password=d*3PSf2MmRX9vJtA5sgwSphCVQ26*T53uU;");
             }
         }
